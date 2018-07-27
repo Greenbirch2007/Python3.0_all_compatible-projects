3944ec58c9acb1ffd40ae675c2bc3380f5eefb38
karson <291109028@qq.com>
2018-07-27 16:35:31 +0800

    7.27 commit

---
+++ docs_spider.py
@@ -0,0 +1,69 @@
+
+import requests
+import re
+import pymysql
+from multiprocessing import Pool
+# 发生异常的处理
+
+
+def get_one_page(url):
+    repseonse = requests.get(url)
+    if repseonse.status_code == 200:
+        return repseonse.text
+    else:
+        return None
+
+
+def parse_one_page(html):
+    patt = re.compile('<a href="/project/.*?/">(.*?)</a>' +
+                      '.*?<p class="package-snippet__description">(.*?)</p>' ,re.S)
+    items = re.findall(patt ,html)
+    content =[]
+    for item in items:
+        content.append(item)
+    return content
+
+
+
+
+# mysql插入数据终于搞定了！
+def insertDB(content):
+    connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='python3_packages',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor()
+    try:
+
+        cursor.executemany("insert into docs (Da,Des) values (%s,%s)", content)
+        connection.commit()
+    except Exception:    # 把异常给放过去了
+        pass
+
+    connection.close()
+
+
+
+
+if __name__ == '__main__':
+    pool = Pool(4)
+    for num in range(35,501):
+        url = 'https://pypi.org/search/?c=Programming+Language+%3A%3A+Python+%3A%3A+3&page=' + str(num)
+        html = get_one_page(url)
+        content = parse_one_page(html)
+        insertDB(content)
+        print(url)
+
+
+
+
+
+
+
+
+
+# insert into docs (Da,Des)  values ('tensorflowonspark', 'Deep learning with TensorFlow on Apache Spark clusters'),('tensorflow-probability-gpu', 'Probabilistic modeling and statistical inference in TensorFlow');
+# create table docs (
+# id int  primary key auto_increment,
+# Da varchar(255) ,
+# Des varchar(255) );
+#
+
